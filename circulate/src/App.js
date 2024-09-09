b04399142af7eb7036cb29b25b9fe85669a4e0c1
Monty1231 <montygoldberg7@gmail.com>
2024-09-09 10:13:02 -0400

    Added register component to app.js

--- circulate/src/App.js
+++ circulate/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Header from "./components/Header.tsx";
+import MarketplacePromo from "./components/register.tsx";
 function App() {
   return (
     <div className="App">
       <Header />
+      <MarketplacePromo />
     </div>
   );
 }
